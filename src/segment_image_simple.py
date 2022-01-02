0706483a240b283fd70e3316e2e8bbf0b1afb1c3
Mateusz Grycmacher <51837956+TheGrycek@users.noreply.github.com>
2022-01-02 15:32:29 +0100

    Scale detection (#1)
    * scale detection added
    
    * scale converted to um
    
    Co-authored-by: Mateusz Grycmacher <mateusz.grycmacher@esportslab.gg>

--- src/segment_image_simple.py
+++ src/segment_image_simple.py
@@ -4,10 +4,12 @@ from pathlib import Path
 import cv2
 import numpy as np
 
+from scale_detector.scale_detector import read_scale
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input_dir", type=Path, default="./images/krio5_OM_1.5_1.jpg",
+    parser.add_argument("-i", "--input_dir", type=Path, default="./images/krio5_OM_2_6.jpg",
                         help="Input image directory.")
 
     return parser.parse_args()
@@ -29,7 +31,7 @@ def crop_rect(img, rect_tilted, rect, cnt):
     center, size, angle = rect_tilted[0], rect_tilted[1], rect_tilted[2]
     small_center = [size[0]/2, size[1]/2]
     translation = [center[0] - small_center[0], center[1] - small_center[1]]
-    print(f"SIZE: {size}, CENTER: {center}, TRAANSLATION {translation}")
+    # print(f"SIZE: {size}, CENTER: {center}, TRAANSLATION {translation}")
     center, size = tuple(map(round, center)), tuple(map(round, size))
     height, width = img.shape[0], img.shape[1]
     M = cv2.getRotationMatrix2D(center, angle, 1)
@@ -73,12 +75,14 @@ def crop_rect(img, rect_tilted, rect, cnt):
 
 
 def main(args):
-    img = cv2.imread(str(args.input_dir), 1)
+    img_original = cv2.imread(str(args.input_dir), 1)
+    img = img_original.copy()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
     cnt, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    for i, c in enumerate(cnt[1:]):
+    i = 0
+    for c in cnt:
         if cv2.contourArea(c) > 1000:
             rect_tilted = cv2.minAreaRect(c)
 
@@ -89,6 +93,11 @@ def main(args):
 
             rect = cv2.boundingRect(c)
             x, y, w, h = rect
+
+            if i == 0:
+                image_scale = read_scale(img_original, rect)
+                print(image_scale)
+
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             img_rot, img_crop, new_c = crop_rect(thresh, rect_tilted, rect, c)
@@ -102,7 +111,7 @@ def main(args):
                 cv2.circle(thresh, tuple(app[0]), 2, [255, 0, 0], -1)
 
             cv2.imwrite(f'./crops/tard_{i}.png', cv2.resize(thresh[y: y + h, x: x + w], (0, 0), fx=2, fy=2))
-
+            i += 1
             thresh = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('oryginal', cv2.resize(img, (0, 0), fx=0.3, fy=0.3))
@@ -112,4 +121,3 @@ def main(args):
 
 if __name__ == '__main__':
     main(parse_args())
-
