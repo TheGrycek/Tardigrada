dataset normalization added, scale bbox detection corrected

src/estimate_biomass.py
@@ -22,33 +22,6 @@ def parse_args():
     return parser.parse_args()
 
 
-def filter_cnts(cnts):
-    cnts_filtered = []
-    bboxes_fit = []
-    bboxes = []
-    for cnt in cnts:
-        if cv2.contourArea(cnt) < 1000:
-            continue
-        rect_tilted = cv2.minAreaRect(cnt)
-        if 0.5 < rect_tilted[1][0] / rect_tilted[1][1] < 2:
-            continue
-        rect = cv2.boundingRect(cnt)
-        bboxes.append(rect)
-        cnts_filtered.append(cnt)
-        bboxes_fit.append(rect_tilted)
-
-    return cnts_filtered, bboxes_fit, bboxes
-
-
-def simple_segmenter(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-    cnts, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    cnts, bboxes_fit, bboxes = filter_cnts(cnts)
-
-    return cnts, bboxes_fit, bboxes, thresh
-
-
 def calc_dist(pt1, pt2):
     return np.sqrt(np.sum(np.square(pt1 - pt2)))
 
@@ -149,9 +122,7 @@ def main(args):
     for img_path in images_paths:
         try:
             img = cv2.imread(str(img_path), 1)
-            cnts, bboxes_fit, bboxes, thresh = simple_segmenter(img)
-
-            image_scale, img = read_scale(img, bboxes, device="cpu")
+            image_scale, img = read_scale(img, device="cpu")
             predicted = predict(model, img, device=cfg.DEVICE)
             results_df, lengths_points = calculate_mass(predicted, image_scale, img_path)
 
@@ -187,7 +158,7 @@ def main(args):
 
             results_df.to_csv(args.output_dir / f"{img_path.stem}_results.csv")
             cv2.imwrite(str(args.output_dir / f"{img_path.stem}_results.jpg"), img)
-            cv2.imshow('predicted', cv2.resize(img, (700, 500)))
+            cv2.imshow('predicted', cv2.resize(img, (1400, 700)))
             cv2.waitKey(2500)
 
         except Exception as e:

src/keypoints_detector/dataset.py
@@ -95,7 +95,7 @@ class KeypointsDataset(Dataset):
             alb.OneOf(one_of, p=0.8),
             alb.OneOf(one_of, p=0.8),
             alb.OneOf(one_of2, p=0.5),
-            # alb.normalize(mean=[], std=[], max_pixel_value=255)
+            alb.Normalize(mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255)
             ],
             keypoint_params=alb.KeypointParams(format='xy', remove_invisible=False),
             bbox_params=alb.BboxParams(format='pascal_voc', min_area=100, label_fields=['bboxes_labels'])
@@ -108,7 +108,7 @@ def collate_function(batch):
     return tuple(zip(*batch))
 
 
-def load_data(images_dir, annotation_file="../coco-1652095945.2305472.json",
+def load_data(images_dir, annotation_file="../coco-1659778596.546996.json",
               transform=True, shuffle=False, val_ratio=cfg.VAL_RATIO, test_ratio=cfg.TEST_RATIO):
     dataset = KeypointsDataset(images_dir=images_dir, annotation_file=annotation_file, transforms=False)
     total_cnt = dataset.__len__()
@@ -129,11 +129,30 @@ def load_data(images_dir, annotation_file="../coco-1652095945.2305472.json",
     return dataloaders
 
 
-if __name__ == '__main__':
-    random.seed(1)
-    dataloader = load_data(images_dir="../images")
+def get_normalization_params(images_dir="../images/train", annotation_file="../coco-1659778596.546996.json"):
+    dataset = KeypointsDataset(images_dir=images_dir, annotation_file=annotation_file, transforms=False)
+    dataloader = DataLoader(dataset=dataset, batch_size=cfg.BATCH_SIZE,
+                            shuffle=False)
+    mean, std, imgs_num = 0, 0, 0
 
-    for i in range(100):
+    for imgs, _ in dataloader:
+        imgs_cnt = imgs.size(0)
+        imgs = imgs.view(imgs_cnt, imgs.size(1), -1)
+        mean += imgs.mean(2).sum(0)
+        std += imgs.std(2).sum(0)
+        imgs_num += imgs_cnt
+
+    mean /= imgs_num
+    std /= imgs_num
+
+    return {"mean": mean.tolist(), "std": std.tolist()}
+
+
+def check_examples():
+    random.seed(1)
+    dataloader = load_data(images_dir="../images/train")["train"]
+
+    for i in range(20):
         for img, annotation in dataloader:
             img = (img[0].numpy() * 255).astype(np.uint8)
             img = np.swapaxes(img, 0, 1)
@@ -149,3 +168,9 @@ if __name__ == '__main__':
 
             cv2.imshow("img", cv2.resize(img, (0, 0), fx=0.5, fy=0.5))
             cv2.waitKey(1500)
+
+
+if __name__ == '__main__':
+    norm_params = get_normalization_params(annotation_file="../coco-1659778596.546996.json")
+    print(f"MEAN:{norm_params['mean']}\nSTD:{norm_params['std']}\n")
+    check_examples()

src/scale_detector/scale_detector.py
@@ -4,8 +4,6 @@ import cv2
 
 
 def change_to_um(text):
-    print(f"MY TEXT: {text}")
-
     scales = {"um": 1,
               "mm": 0.001,
               "cm": 0.0001}
@@ -20,38 +18,86 @@ def change_to_um(text):
     return value
 
 
-def read_scale(img, bboxes, device="cpu"):
-    if len(bboxes) == 0:
-        return {"pix": 0, "um": 0}, img
+def calc_overlap_perc(bbox1, bbox2):
+    x11, x12, y11, y12 = bbox1
+    x21, x22, y21, y22 = bbox2
+    s1 = (abs(x11 - x12) * abs(y11 - y12))
+    si = max(0, min(x12, x22) - max(x11, x21)) * max(0, min(y12, y22) - max(y11, y21))
+    su = s1 - si
+    return 1 if su == 0 else si / su
 
+
+def filter_cnts(cnts, scale_pts):
+    cnts_filtered = []
+    bboxes_fit = []
+    bboxes = []
+    p1, p2, p3, p4, = scale_pts
+    l, h = abs(p1[0] - p2[0]), abs(p1[1] - p4[1])
+    text_len = max(l, h)
+
+    for cnt in cnts:
+        rect = cv2.boundingRect(cnt)
+        bbox_len = rect[2] if text_len == l else rect[3]
+        if bbox_len < text_len:
+            continue
+        overlap_perc = calc_overlap_perc([p1[0], p2[0], p1[1], p4[1]],
+                                         [rect[0], rect[0] + rect[2], rect[1], rect[1] + rect[3]])
+        if overlap_perc > 0.5:
+            continue
+
+        rect_tilted = cv2.minAreaRect(cnt)
+        if 0.5 < rect_tilted[1][0] / rect_tilted[1][1] < 2:
+            continue
+
+        bboxes.append(rect)
+        cnts_filtered.append(cnt)
+        bboxes_fit.append(rect_tilted)
+
+    return cnts_filtered, bboxes_fit, bboxes
+
+
+def simple_segmenter(img, scale_pts):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+    cnts, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cnts, bboxes_fit, bboxes = filter_cnts(cnts, scale_pts)
+
+    return cnts, bboxes_fit, bboxes, thresh
+
+
+def select_bbox(bboxes, scale_pts, img):
+    p1, p2, p3, p4 = scale_pts
+    text_center = np.array([abs(p1[0] - p2[0]) / 2 + p1[0], abs(p1[1] - p4[1]) / 2 + p1[1]])
+
+    def calc_dist(bbox):
+        x, y, w, h = bbox
+        bbox_center = np.array([round(x + w / 2), round(y + h / 2)])
+        return np.power(np.sum((bbox_center - text_center) ** 2), 0.5)
+
+    bboxes = sorted(bboxes, key=calc_dist, reverse=False)
+    for x, y, w, h in bboxes:
+        img = cv2.rectangle(img.copy(), (x, y), (x + w, y + h), (0, 0, 255), 2)
+    return bboxes[0]
+
+
+def read_scale(img, device="cpu"):
     reader = Reader(['en'], gpu=True if device == "gpu" else False)
-
-    # TODO: create bboxes filtering algorithm
-    x, y, w, h = bboxes[0]
     result = reader.readtext(img,
                              allowlist=["u", "m", "c"] + [str(i) for i in range(10)],
                              slope_ths=5,
                              width_ths=5)
 
     text_threshold = 0.8
-    scale_center = np.array([round(x + w/2), round(y + h/2)])
-
-    def calc_dist(res):
-        p1, p2, p3, p4 = res[0]
-        centers = np.array([abs(p1[0] - p2[0]) / 2 + p1[0],
-                            abs(p1[1] - p4[1]) / 2 + p1[1]])
-
-        return np.power(np.sum((scale_center - centers) ** 2), 0.5)
 
     result = filter(lambda res: res[1][-1] == "m", result)
-    result = filter(lambda res: res[2] >= text_threshold, result)
-    result = sorted(result, key=calc_dist, reverse=True)
-    print(f"SCALE RESULT: {result}")
+    result = list(filter(lambda res: res[2] >= text_threshold, result))
+    cnts, bboxes_fit, bboxes, thresh = simple_segmenter(img, result[0][0])
 
-    if len(result) == 0:
+    if len(bboxes) == 0 or len(result) == 0:
         return {"pix": 0, "um": 0}, img
 
     scale_value = change_to_um(result[0][1])
+    x, y, w, h = select_bbox(bboxes, result[0][0], img)
 
     img = cv2.rectangle(img.copy(), (x, y), (x + w, y + h), (0, 0, 255), 2)
     img = cv2.putText(img, 'scale', (x, y), cv2.FONT_HERSHEY_SIMPLEX,